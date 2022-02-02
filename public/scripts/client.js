9a4b1fe82b951860a203373f8cae14f7aee8d6df
Olivia <olivia.cowan@hotmail.com>
2022-02-02 23:27:15 +0000

    add GET to fetch data from server

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,32 +5,6 @@
  */
 
 $(() => {
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
- 
  
   const createTweetElement = function (data) {
 
@@ -76,22 +50,32 @@ $(() => {
     })
     return;
   }
-  renderTweets(data);
 
+  const loadNewTweets = function() {
+    $.ajax({
+      url: '/tweets',
+      method: 'GET'
+    }).then((tweets) => {
+      $('.tweetcontainer').empty();
+      //console.log('tweeeets', tweets);
+      renderTweets(tweets);
+    })
+  }
+  loadNewTweets();
 
   $("#submit-form").on('submit', function(event) {
     event.preventDefault();
-    console.log('entered')
-    //console.log(this);
+    //console.log('entered')
     const data = $(this).serialize();
     console.log('data', data)
-
+    $('form').trigger('reset');
+    
     $.ajax({
       method: 'POST',
       url: '/tweets',
       data: data
     }).then(()=>{
-      console.log('success');
+      //console.log('success');
     })
   });
 })
